91a929a0b1b7c0cc7eaed06afd6ce8eca5263828
tevo.castro14@gmail.com <tevo.castro14@gmail.com>
2024-01-22 13:12:24 -0300

    update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  
+
   $('#painel').on('submit', function (e) {
     e.preventDefault();
     
     const escrevaTarefa = $('#escrever-tarefa').val();
     
+    // Adicionar novo item ao HTML
     const novoItem = $(`
     <tr>
       <td>
@@ -12,21 +13,25 @@ $(document).ready(function() {
       </td>
       <td><p>${escrevaTarefa}</p></td>
       <td>
-        <button style="border-radius:90px;"><i class="fa-solid fa-xmark"></i></button>
+        <button style="border-radius:90px;"><i class="fas fa-times"></i></button>
       </td>
     </tr>`);
     
     $('tbody').append(novoItem);
     
     $('#escrever-tarefa').val('');
+
+    // Salvar tarefas após adicionar uma nova
+    saveTasks();
   })
   
   $('tbody').on('click', 'button', function() {
     $(this).closest('tr').remove();
+    saveTasks();
   });
   
+  // Mudar o texto para riscado quando seleciona
   $('tbody').on('change', 'input[type="checkbox"]', function() {
-    
     var checkbox = $(this); 
     var paragrafo = checkbox.closest('tr').find('p');
     if (checkbox.prop('checked')) {
@@ -34,8 +39,24 @@ $(document).ready(function() {
     } else {
       paragrafo.css('text-decoration', 'none');
     }
+    saveTasks();
   });
+  
+  // Remover todas as tarefas adicionadas
   $('#remover-tarefa').on('click', function() {
     $('tbody').empty();
+    saveTasks();
   });
-})
+  
+  // Função para salvar tarefas
+  function saveTasks() {
+    var tasksHtml = $('tbody').html();
+    localStorage.setItem('tasks', tasksHtml);
+  }
+  
+  // Restaurar tarefas salvas ao carregar a página
+  var savedTasks = localStorage.getItem('tasks');
+  if (savedTasks) {
+    $('tbody').html(savedTasks);
+  }
+});
